2174c743a2a2ec257dcd88f1fa368d3462fa0321
sidali <sidalidady@gmail.com>
2025-10-10 21:47:41 +0200

    style: Make Users on dApp section more compact with smaller text and avatars

--- frontend/src/components/OnlineUsers.js
+++ frontend/src/components/OnlineUsers.js
@@ -110,7 +110,7 @@ function AllUsers() {
             {profileImage ? (
               <img src={profileImage} alt={username} />
             ) : (
-              <User size={28} />
+              <User size={20} />
             )}
           </div>
         </div>
@@ -125,7 +125,7 @@ function AllUsers() {
   return (
     <div className="online-users-section">
       <h3 className="online-users-title">
-        <Users size={20} />
+        <Users size={16} />
         Users on dApp ({allUsers.length})
       </h3>
       <div className="online-users-list">
