3c3cad62c8e60550cba7a6ab17f178953dbf85f3
sidali <sidalidady@gmail.com>
2025-10-11 23:37:01 +0200

    Add Helios Social as second line under Stellari

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -458,6 +458,7 @@ function Header({ onProfileClick, onConnectClick, onSearch }) {
             <SunLogo />
             <div className="header-brand">
               <div className="brand-name">Stellari</div>
+              <div className="brand-subtitle">Helios Social</div>
             </div>
           </div>
 
