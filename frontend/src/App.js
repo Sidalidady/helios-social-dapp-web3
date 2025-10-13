e347b90bac06cc8cdd0069e59a285cb651483918
sidali <sidalidady@gmail.com>
2025-10-13 23:34:24 +0200

    chore: update Twitter profile link and handle to @Stellari_social

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -878,11 +878,11 @@ function AppContent() {
 
       {/* Twitter Profile Button */}
       <a 
-        href="https://x.com/dadi91429" 
+        href="https://x.com/Stellari_social" 
         target="_blank" 
         rel="noopener noreferrer"
         className="twitter-profile-button"
-        title="Follow @dadi91429 on X (Twitter)"
+        title="Follow @Stellari_social on X (Twitter)"
       >
         <svg viewBox="0 0 24 24" aria-hidden="true">
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
